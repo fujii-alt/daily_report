ea8a4f7e724861b17d7a7c1dafeb816d3d17cf18
knowthyself <fujii@g-knowthyself.com>
2026-05-19 10:34:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'https://script.google.com/a/macros/g-knowthyself.com/s/AKfycbx1gULMjLGD0yjbxMA0x9m6B8P9dJKLlqMY91lrQdKhBONj0NSkXg96I9pXOSZfMihb/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycby8a6l_QqAJUWlyZM3fhqrtNTsnyBz6eTdC69nggdonLLVVT881DLMgopEJ-ulGWHhf/exec',
   STORAGE_KEY: 'daily-report-app-session',
   APP_NAME: '日報入力',
 };
